64efc73938e104c45f8fb91f1e2acc9c031d8697
Zackery Rilling <rillingz@yahoo.com>
2017-04-19 14:04:55 -0700

    Javascript, Data, and Routes Set up.

--- server.js
+++ server.js
@@ -4,4 +4,14 @@ var path = require("path");
 
 var app = express();
 
-var PORT = 8080;
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT);
